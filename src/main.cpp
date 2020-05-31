78dfba2809b41c350e886536cf1e659d7fa4cb73
Eoin Mcloughlin <eoinmcl@netsoc.tcd.ie>
2020-05-31 12:57:00 +0100

    mmap bookmarks, so they are saved/loaded across runs

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,55 @@
 #include <XlibKeyConnection.h>
 #include <iostream>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+
+// Return \a numMarks writable Bookmarks stored in \a path, creating
+// the file if it does not exist.
+std::optional<Bookmark>* getBookmarkData(const char* path, int numMarks)
+{
+	int filefd = open(path, O_CREAT | O_RDWR, 0666);
+	if(filefd < 0)
+		return nullptr;
+	const unsigned markSize = sizeof(std::optional<Bookmark>);
+
+	// Ensure a minimum file size by writing up to numMarks empty marks:
+	while(lseek(filefd, 0, SEEK_END) < markSize * numMarks)
+	{
+		char empty[markSize] = {0};
+		write(filefd, empty, markSize);
+	}
+
+	lseek(filefd, 0, SEEK_SET);
+	void* data = mmap(nullptr, markSize * numMarks, PROT_READ | PROT_WRITE, MAP_SHARED, filefd, 0);
+	close(filefd);
+	return reinterpret_cast<std::optional<Bookmark>*>(data);
+}
 
 int main()
 {
 	GqrxConnection a;
 	XlibKeyConnection b;
 
-	std::optional<Bookmark> marks[12];
+	const char* savePath = "/home/eoin/.config/gqrx/savedMarks.dat";
+	const int numMarks = 12;
+	auto marks = getBookmarkData(savePath, numMarks);
 
 	b.m_memoryCallback = [&](XlibKeyConnection::Mode mode, int slot)
 	{
 		if(mode == XlibKeyConnection::Mode::SAVE)
 		{
 			Bookmark cur;
-			std::cout << "Saving to slot " << slot << "\n";
 			if(a.getMark(cur) == GqrxConnection::Result::SUCCESS)
 			{
 				marks[slot] = cur;
-				std::cout << "\t" << marks[slot]->m_frequency << "Hz\n";
+				std::cout << "Saving " << cur.m_frequency << " to slot " << slot << "\n";
+			}
+			else
+			{
+				std::cout << "gqrx connection error\n";
 			}
 		}
 		else if(mode == XlibKeyConnection::Mode::LOAD && marks[slot].has_value())
